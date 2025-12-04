c02f4bd88c1e54a2be0699f03986499e82a30a0a
Ariano2700 <kawuai123loco123@gmail.com>
2025-12-04 12:31:31 -0500

    fix: Update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://100panas.netlify.app",
   vite: {
     plugins: [tailwindcss()],
   },
